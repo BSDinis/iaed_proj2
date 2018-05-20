7c32a0942f8ead37b55bffd86aea43f7b887e09f
Baltasar Dinis <baltasardinis@gmail.com>
2018-05-20 12:09:30 +0100

    separated the command datatype

---
+++ cmd.h
@@ -0,0 +1,16 @@
+/*
+ *
+ * cmd.h
+ *
+ * Baltasar Dinis 89416
+ * IAED project
+ *
+ * defines the cmd datatype
+ */
+
+#ifndef CMD_H
+#define CMD_H
+
+typedef enum {ADD = 0, DUR, DEP, RM, PATH, EXIT, INVALID} cmd;
+
+#endif /* !CMD_H */
